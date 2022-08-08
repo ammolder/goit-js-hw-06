704b2e87c20def8925f5a41b03404cdd984b64a3
ammolder <98980721+ammolder@users.noreply.github.com>
2022-08-08 18:25:43 +0200

    changeLocalFile

--- js/task-04.js
+++ js/task-04.js
@@ -2,11 +2,9 @@ const decrement = document.querySelector('[data-action="decrement"]');
 const increment = document.querySelector('[data-action="increment"]');
 const spanEl = document.querySelector("#value");
 let counterValue = 0;
-console.log(spanEl);
 
 decrement.addEventListener("click", onDecrementValue);
 increment.addEventListener("click", onIncrementValue);
-// console.log(onDecrementValue());
 
 function onDecrementValue(evt) {
   counterValue -= 1;
